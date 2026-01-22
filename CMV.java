d7716b6f9594acf998095efda05d5f32ddb6c84d
Vadim <vadim.elguedj@epfl.ch>
2026-01-22 20:05:40 +0100

    Add function lic9 (Fix #9)

--- CMV.java
+++ CMV.java
@@ -12,7 +12,7 @@ public class CMV {
         cmv[6] = lic6();
         cmv[7] = lic7();
         cmv[8] = lic8();
-        cmv[9] = lic9();
+        cmv[9] = lic9(points, parameters);
         cmv[10] = lic10();
         cmv[11] = lic11();
         cmv[12] = lic12();
@@ -31,7 +31,45 @@ public class CMV {
     public static Boolean lic6() {return false;}
     public static Boolean lic7() {return false;}
     public static Boolean lic8() {return false;}
-    public static Boolean lic9() {return false;}
+
+    public static Boolean lic9(Point[] points, Parameters parameters) {
+        int i = 0;
+        if(points.length < 5) return false;
+
+        for (Point A : points) {
+            if(i + parameters.C_PTS + parameters.D_PTS + 2 < points.length) {
+                Point B = points[i + parameters.C_PTS + 1];
+                Point C = points[i + parameters.C_PTS + parameters.D_PTS + 2];
+
+                if(!A.areTheSame(B) && !C.areTheSame(B)) {
+
+                    double BAx = A.x - B.x;
+                    double BAy = A.y - B.y;
+
+                    double BCx = C.x - B.x;
+                    double BCy = C.y - B.y;
+
+                    double dotProduct = BAx*BCx + BAy*BCy;
+
+                    double norm_BA = A.distance(B);
+                    double norm_BC = C.distance(B);
+
+                    if(norm_BA > 0 && norm_BC > 0) {
+                        double cosValue = dotProduct / (norm_BA * norm_BC);
+                        cosValue = Math.max(-1.0, Math.min(1.0, cosValue));
+                        double angle = Math.acos(cosValue);
+
+                        if(angle < Parameters.PI - parameters.EPSILON || angle > Parameters.PI + parameters.EPSILON) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            i = i + 1;
+        }
+        return false;
+    }
+
     public static Boolean lic10() {return false;}
     public static Boolean lic11() {return false;}
     public static Boolean lic12() {return false;}
