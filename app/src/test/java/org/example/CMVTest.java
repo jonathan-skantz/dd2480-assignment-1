6fcfc120300e0f5dc1867065db580770c720d3b8
Vadim <vadim.elguedj@epfl.ch>
2026-01-24 21:38:28 +0100

    Add function lic10 (Fix #11)

--- app/src/test/java/org/example/CMVTest.java
+++ app/src/test/java/org/example/CMVTest.java
@@ -79,4 +79,56 @@ class CMVTest {
 
         assertTrue(CMV.lic9(points, C_PTS, D_PTS, EPSILON));
     }
+
+    @Test
+    void lic10_enoughPoint_returnsFalse() {
+        Point[] points = {
+                new Point(0,0),
+                new Point(1, 0),
+                new Point(2, 0),
+                new Point(3, 0)
+        };
+
+        int E_PTS = 1;
+        int F_PTS = 1;
+        double AREA1 = 2.2;
+
+        assertFalse(CMV.lic10(points, E_PTS, F_PTS, AREA1));
+    }
+
+    @Test
+    void lic10_conditionsMet_returnsTrue() {
+        Point[] points = {
+                new Point(0, 1),
+                new Point(0, 2),
+                new Point(0, 0),
+                new Point(1, 0),
+                new Point(3, 0),
+                new Point(3,0)
+        };
+
+        int E_PTS = 1;
+        int F_PTS = 2;
+        double AREA1 = 1.49;
+
+        assertTrue(CMV.lic10(points, E_PTS, F_PTS, AREA1));
+    }
+
+    @Test
+    void lic10_conditionsNotMet_returnsFalse () {
+        Point[] points = {
+                new Point(0, 1),
+                new Point(0, 2),
+                new Point(0, 0),
+                new Point(1, 0),
+                new Point(3, 0),
+                new Point(3,0)
+        };
+
+        int E_PTS = 1;
+        int F_PTS = 2;
+        double AREA1 = 1.51;
+
+        assertFalse(CMV.lic10(points, E_PTS, F_PTS, AREA1));
+    }
 }
