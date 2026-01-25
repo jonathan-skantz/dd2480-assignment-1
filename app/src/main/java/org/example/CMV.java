97c9644461ef00778ec101461e84942cf68c1d55
Vadim <vadim.elguedj@epfl.ch>
2026-01-25 18:21:41 +0100

    Add function lic12 (Fix #7)

--- app/src/main/java/org/example/CMV.java
+++ app/src/main/java/org/example/CMV.java
@@ -18,7 +18,7 @@ public class CMV {
         cmv[9] = lic9(points, parameters.C_PTS, parameters.D_PTS, parameters.EPSILON);
         cmv[10] = lic10(points, parameters.E_PTS, parameters.F_PTS, parameters.AREA1);
         cmv[11] = lic11();
-        cmv[12] = lic12();
+        cmv[12] = lic12(points, parameters.K_PTS, parameters.LENGTH1, parameters.LENGTH2);
         cmv[13] = lic13();
         cmv[14] = lic14();
 
@@ -108,7 +108,39 @@ public class CMV {
     }
 
     public static Boolean lic11() {return false;}
-    public static Boolean lic12() {return false;}
+
+
+    /**
+     * LIC 12: distance > LENGTH1 for two data points separated by exactly K_PTS and distance > LENGTH2 for two other
+     * (or the same) data points separated also by exactly K_PTS
+     * @param points Input parameter
+     * @param K_PTS Input parameter
+     * @param LENGTH1 Input parameter
+     * @param LENGTH2 Input parameter
+     * @return true if condition is met
+     */
+    public static Boolean lic12(Point[] points, int K_PTS, double LENGTH1, double LENGTH2) {
+        int i = 0;
+        boolean condition_1 = false;
+        boolean condition_2 = false;
+        if(points.length < 3) {return false;}
+
+        for (Point A : points) {
+            if(i + K_PTS + 1 < points.length) {
+                Point B = points[i + K_PTS + 1];
+                double distance = A.distance(B);
+                if(distance > LENGTH1) {
+                    condition_1 = true;
+                }
+                if(distance < LENGTH2) {
+                    condition_2 = true;
+                }
+            }
+            i += 1;
+        }
+        return condition_1 && condition_2;
+    }
+
     public static Boolean lic13() {return false;}
     public static Boolean lic14() {return false;}
 
--- app/src/test/java/org/example/CMVTest.java
+++ app/src/test/java/org/example/CMVTest.java
@@ -131,4 +131,56 @@ class CMVTest {
 
         assertFalse(CMV.lic10(points, E_PTS, F_PTS, AREA1));
     }
+
+    @Test
+    void lic12_enoughPoint_returnsFalse() {
+        Point[] points = {
+                new Point(0,0),
+                new Point(1, 0),
+                new Point(2, 0),
+                new Point(3, 0)
+        };
+
+        int K_PTS = 2;
+        double LENGTH1 = 2.0;
+        double LENGTH2 = 3.0;
+
+        assertFalse(CMV.lic12(points, K_PTS, LENGTH1, LENGTH2));
+    }
+
+    @Test
+    void lic12_conditionsMet_returnsTrue() {
+        Point[] points = {
+                new Point(0, 1),
+                new Point(0, 2),
+                new Point(0, 0),
+                new Point(0, 3.1),
+                new Point(3, 0),
+                new Point(3,0),
+                new Point(0,6.2)
+        };
+        int K_PTS = 2;
+        double LENGTH1 = 2.0;
+        double LENGTH2 = 3.0;
+
+        assertTrue(CMV.lic12(points, K_PTS, LENGTH1, LENGTH2));
+    }
+
+    @Test
+    void lic12_conditionsMet2_returnsTrue() {
+        Point[] points = {
+                new Point(0, 1),
+                new Point(0, 2),
+                new Point(0, 0),
+                new Point(0, 3.1),
+                new Point(3, 0),
+                new Point(0.1,0),
+                new Point(0,6.2)
+        };
+        int K_PTS = 2;
+        double LENGTH1 = 2.0;
+        double LENGTH2 = 1.0;
+
+        assertTrue(CMV.lic12(points, K_PTS, LENGTH1, LENGTH2));
+    }
 }
