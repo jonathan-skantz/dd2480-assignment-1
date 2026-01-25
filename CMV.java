95e8be1e00fa665c922b18fc766a16802bd5f729
FabianHolm98 <fabian.holm@hotmail.com>
2026-01-25 10:23:25 +0100

    Add LIC1 by creating method

--- CMV.java
+++ CMV.java
@@ -4,7 +4,7 @@ public class CMV {
         Boolean[] cmv = new Boolean[15];
 
         cmv[0] = lic0();
-        cmv[1] = lic1();
+        cmv[1] = lic1(points, parameters.RADIUS1);
         cmv[2] = lic2();
         cmv[3] = lic3();
         cmv[4] = lic4();
@@ -23,7 +23,42 @@ public class CMV {
     }
 
     public static Boolean lic0() {return false;}
-    public static Boolean lic1() {return false;}
+
+    public static Boolean lic1(Point[] points, double radius1) {
+
+        // No need to check if there are no trio of points
+        if (radius1 < 0) return false;
+        if (points.length < 3) return false; 
+
+        for (int i = 0; i < points.length - 2; i++) {
+            Point p1 = points[i];
+            Point p2 = points[i + 1];
+            Point p3 = points[i + 2];
+
+            double d1 = p1.distance(p2);
+            double d2 = p2.distance(p3);
+            double d3 = p3.distance(p1);
+
+            double[] arr = {d1, d2, d3};
+            double minRadius;
+            Arrays.sort(arr);
+
+            if (Math.pow(arr[2], 2) >= Math.pow(arr[1], 2) + Math.pow(arr[0], 2)) {
+                // Obtuse/Right triangle
+                minRadius = arr[2] / 2;
+            } else {
+                // Acute triangle
+                double s = (d1 + d2 + d3) / 2;
+                double area = Math.sqrt(s*(s-d1)*(s-d2)*(s-d3));
+                minRadius = (d1 * d2 * d3) / (4 * area);
+            }
+
+            if (minRadius > radius1) return true; 
+        }
+
+        return false; // No trio that fits the requirements found
+    }
+
     public static Boolean lic2() {return false;}
     public static Boolean lic3() {return false;}
     public static Boolean lic4() {return false;}
