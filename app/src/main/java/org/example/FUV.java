4cbf6039bf13d418e912c4839cb8da1b878008ac
eliashollstrand <elias.hollstrand@icloud.com>
2026-01-28 19:55:29 +0100

    Add computeFUV method and tests. Fix #75

--- app/src/main/java/org/example/FUV.java
+++ app/src/main/java/org/example/FUV.java
@@ -3,6 +3,26 @@ package org.example;
 public class FUV {
 
     /**
+    * Computes the Final Unlocking Vector (FUV) - A vector calculated from the
+    * Preliminary Unlocking Vector (PUV) and the Preliminary Unlocking Matrix (PUM).
+    *
+    * @param puv the Preliminary Unlocking Vector (PUV)
+    * @param pum the Preliminary Unlocking Matrix (PUM)
+    * @return the Final Unlocking Vector (FUV)
+    */
+    public static boolean[] computeFUV(boolean[] puv, boolean[][] pum) {
+        int fuv_length = puv.length;
+        boolean[] fuv = new boolean[fuv_length];
+
+        for(int i = 0; i < fuv_length; i++) {
+            if(!puv[i]) fuv[i] = true;
+            else fuv[i] = isAllTrue(pum[i]);
+        }
+
+        return fuv;
+    }
+
+    /**
      * Checks if all elements in a boolean array are true.
      * 
      * @param arr the boolean array
