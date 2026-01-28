4ae94e335872b0c8f5c852380f72c050df4f44c1
FabianHolm98 <fabian.holm@hotmail.com>
2026-01-28 10:17:01 +0100

    fixed merge conflicts and put tests in order

--- app/src/test/java/org/example/CMVTest.java
+++ app/src/test/java/org/example/CMVTest.java
@@ -12,6 +12,118 @@ class CMVTest {
     }
     
     /**
+     * If {@code RADIUS1} is negative, lic1 should return false.
+     */
+    @Test
+    void lic1_negativeRadius_returnsFalse() {
+
+        Point[] points = {
+            new Point(0, 0),
+            new Point(1, 0),
+            new Point(0, 1)
+        };
+
+        assertFalse(CMV.lic1(points, -1.0));
+    }
+
+    /**
+     * If less than three points in input, lic1 should return false.
+     */
+    @Test
+    void lic1_lessThanThreePoints_returnsFalse() {
+        Point[] points = {
+            new Point(0, 0),
+            new Point(1, 0)
+        };
+
+        assertFalse(CMV.lic1(points, 1));
+    }
+
+    /**
+     * If three consecutive points create an Obtuse/Right triangle that cannot fitt incide a cirecle with {@code RADIUS1}, lic1 should return true.
+     */
+    @Test
+    void lic1_obtuseRightTriangleMinimumRadiusGreaterThanRADIUS1_returnsTrue() {
+        Point[] points = {
+            new Point(0, 0),
+            new Point(3, 0),
+            new Point(0, 4)
+        };
+
+        assertTrue(CMV.lic1(points, 2.0));
+    }
+
+    /**
+     * If three consecutive points create an Obtuse/Right triangle that creates a minimum radius that is equal to {@code RADIUS1}, lic1 should return false.
+     */
+    @Test
+    void lic1_obtuseRightTriangleMinimumRadiusEqualToRADIUS1_returnsFalse() {
+        Point[] points = {
+            new Point(0, 0),
+            new Point(3, 0),
+            new Point(0, 4)
+        };
+
+        assertFalse(CMV.lic1(points, 2.5));
+    }
+
+    /**
+     * If three consecutive points create an Obtuse/Right triangle that can fitt incide a cirecle with {@code RADIUS1}, lic1 should return false.
+     */
+    @Test
+    void lic1_obtuseRightTriangleMinimumRadiusSmallerThanRADIUS1_returnsFalse() {
+        Point[] points = {
+            new Point(0, 0),
+            new Point(3, 0),
+            new Point(0, 4)
+        };
+
+        assertFalse(CMV.lic1(points, 3.0));
+    }
+
+    /**
+     * If three consecutive points create an acute triangle that cannot fitt incide a cirecle with {@code RADIUS1}, lic1 should return true.
+     */
+    @Test
+    void lic1_acuteTriangleMinimumRadiusGreaterThanRADIUS1_returnsTrue() {
+        Point[] points = {
+            new Point(0, 0),
+            new Point(2, 0),
+            new Point(1, Math.sqrt(3))
+        };
+
+        assertTrue(CMV.lic1(points, 1.0));
+    }
+
+    /**
+     * If three consecutive points create an acute triangle that creates a minimum radius that is equal to {@code RADIUS1}, lic1 should return false.
+     */
+    @Test
+    void lic1_acuteTriangleMinimumRadiusEqualToRADIUS1_returnsFalse() {
+        Point[] points = {
+            new Point(0, 0),
+            new Point(2, 0),
+            new Point(1, Math.sqrt(3))
+        };
+
+        assertFalse(CMV.lic1(points, (2.0 / Math.sqrt(3))));
+    }
+
+    /**
+     * If three consecutive points create an acute triangle that can fitt incide a cirecle with {@code RADIUS1}, lic1 should return false.
+     */
+    @Test
+    void lic1_acuteTriangleMinimumRadiusSmallerThanRADIUS1_returnsFalse() {
+        Point[] points = {
+            new Point(0, 0),
+            new Point(2, 0),
+            new Point(1, Math.sqrt(3))
+        };
+
+        assertFalse(CMV.lic1(points, 1.3));
+    }
+
+    /**
      * If {@code AREA1} is negative, lic3 should return false.
      */
     @Test
@@ -431,118 +543,4 @@ class CMVTest {
         assertFalse(CMV.lic7(points, 1, -1.0));
     }
 
-    }
-
-    /**
-     * If {@code RADIUS1} is negative, lic1 should return false.
-     */
-    @Test
-    void lic1_negativeRadius_returnsFalse() {
-
-        Point[] points = {
-            new Point(0, 0),
-            new Point(1, 0),
-            new Point(0, 1)
-        };
-
-        assertFalse(CMV.lic1(points, -1.0));
-    }
-
-    /**
-     * If less than three points in input, lic1 should return false.
-     */
-    @Test
-    void lic1_lessThanThreePoints_returnsFalse() {
-        Point[] points = {
-            new Point(0, 0),
-            new Point(1, 0)
-        };
-
-        assertFalse(CMV.lic1(points, 1));
-    }
-
-    /**
-     * If three consecutive points create an Obtuse/Right triangle that cannot fitt incide a cirecle with {@code RADIUS1}, lic1 should return true.
-     */
-    @Test
-    void lic1_obtuseRightTriangleMinimumRadiusGreaterThanRADIUS1_returnsTrue() {
-        Point[] points = {
-            new Point(0, 0),
-            new Point(3, 0),
-            new Point(0, 4)
-        };
-
-        assertTrue(CMV.lic1(points, 2.0));
-    }
-
-    /**
-     * If three consecutive points create an Obtuse/Right triangle that creates a minimum radius that is equal to {@code RADIUS1}, lic1 should return false.
-     */
-    @Test
-    void lic1_obtuseRightTriangleMinimumRadiusEqualToRADIUS1_returnsFalse() {
-        Point[] points = {
-            new Point(0, 0),
-            new Point(3, 0),
-            new Point(0, 4)
-        };
-
-        assertFalse(CMV.lic1(points, 2.5));
-    }
-
-    /**
-     * If three consecutive points create an Obtuse/Right triangle that can fitt incide a cirecle with {@code RADIUS1}, lic1 should return false.
-     */
-    @Test
-    void lic1_obtuseRightTriangleMinimumRadiusSmallerThanRADIUS1_returnsFalse() {
-        Point[] points = {
-            new Point(0, 0),
-            new Point(3, 0),
-            new Point(0, 4)
-        };
-
-        assertFalse(CMV.lic1(points, 3.0));
-    }
-
-    /**
-     * If three consecutive points create an acute triangle that cannot fitt incide a cirecle with {@code RADIUS1}, lic1 should return true.
-     */
-    @Test
-    void lic1_acuteTriangleMinimumRadiusGreaterThanRADIUS1_returnsTrue() {
-        Point[] points = {
-            new Point(0, 0),
-            new Point(2, 0),
-            new Point(1, Math.sqrt(3))
-        };
-
-        assertTrue(CMV.lic1(points, 1.0));
-    }
-
-    /**
-     * If three consecutive points create an acute triangle that creates a minimum radius that is equal to {@code RADIUS1}, lic1 should return false.
-     */
-    @Test
-    void lic1_acuteTriangleMinimumRadiusEqualToRADIUS1_returnsFalse() {
-        Point[] points = {
-            new Point(0, 0),
-            new Point(2, 0),
-            new Point(1, Math.sqrt(3))
-        };
-
-        assertFalse(CMV.lic1(points, (2.0 / Math.sqrt(3))));
-    }
-
-    /**
-     * If three consecutive points create an acute triangle that can fitt incide a cirecle with {@code RADIUS1}, lic1 should return false.
-     */
-    @Test
-    void lic1_acuteTriangleMinimumRadiusSmallerThanRADIUS1_returnsFalse() {
-        Point[] points = {
-            new Point(0, 0),
-            new Point(2, 0),
-            new Point(1, Math.sqrt(3))
-        };
-
-        assertFalse(CMV.lic1(points, 1.3));
-    }
-   
 }
