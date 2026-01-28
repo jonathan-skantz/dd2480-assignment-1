4d4147702ae276856cc775109d62970716f1fa19
eliashollstrand <elias.hollstrand@icloud.com>
2026-01-28 17:17:23 +0100

    Change Boolean[] to boolean[] in order for input to computePUM to be of correct type. Fix #72 #31

--- app/src/main/java/org/example/Decide.java
+++ app/src/main/java/org/example/Decide.java
@@ -4,7 +4,7 @@ public class Decide {
     public String DECIDE(int NUMPOINTS, Point[] POINTS, Parameters PARAMETERS, Connector[][] LCM, boolean[] PUV) {
         String LAUNCH = "NO";
 
-        Boolean[] cmv = CMV.computeCMV(POINTS, PARAMETERS);
+        boolean[] cmv = CMV.computeCMV(POINTS, PARAMETERS);
         boolean[][] pum = PUM.computePUM(LCM, cmv);
     
         return LAUNCH;
