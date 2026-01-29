c1c306f72a4fbb4ac62fe560ca031063e3d64fdc
Elias Hollstrand <84776154+eliashollstrand@users.noreply.github.com>
2026-01-29 09:00:31 +0100

    Refactor DECIDE for cleaner function body. Fix #88
    Co-authored-by: Jonathan Skantz <57753056+jonathan-skantz@users.noreply.github.com>

--- app/src/main/java/org/example/Decide.java
+++ app/src/main/java/org/example/Decide.java
@@ -2,7 +2,11 @@ package org.example;
 
 public class Decide {
     public String DECIDE(int NUMPOINTS, Point[] POINTS, Parameters PARAMETERS, Connector[][] LCM, boolean[] PUV) {
-        String LAUNCH = "NO";
+        boolean[] cmv = CMV.computeCMV(POINTS, PARAMETERS);
+        boolean[][] pum = PUM.computePUM(LCM, cmv);
+        boolean[] fuv = FUV.computeFUV(PUV, pum);
+    
+        return FUV.isAllTrue(fuv) ? "YES" : "NO";
 
         boolean[] cmv = CMV.computeCMV(POINTS, PARAMETERS);
         boolean[][] pum = PUM.computePUM(LCM, cmv);
